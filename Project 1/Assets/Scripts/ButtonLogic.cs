3ec474a762d173a1a414301e625c6070fd9778c8
jacgoldberg <59575988+jacgoldberg@users.noreply.github.com>
2021-02-12 10:17:59 -0800

    Loading Files

---
+++ Project 1/Assets/Scripts/ButtonLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLogic : MonoBehaviour
+{
+    //Activates on press of ingame button.
+   public void onButtonPress()
+    {
+        //Opens and Writes to logFile
+        using (System.IO.StreamWriter logFile = new System.IO.StreamWriter(@"C:\Users\goldb\Desktop\LogFile.txt", true))
+        {
+            logFile.WriteLine("["
+                + System.DateTime.Now
+                + "]"
+                + "Button Pressed"
+                + "\n");
+        }
+    }
+}
---
+++ Project 1/Assets/Scripts/ButtonTracker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTracker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Clears File at game start up
+        System.IO.File.WriteAllText(@"C:\Users\goldb\Desktop\LogFile.txt", string.Empty);
+        WriteToLog("System Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Logic for checking when space bar is pressed
+        if (Input.GetButtonDown("Jump"))
+        {
+            WriteToLog("Space Bar Pressed");        
+        }
+    }
+    //Writes to logFile
+    void WriteToLog(string message)
+    {
+        using(System.IO.StreamWriter logFile = new System.IO.StreamWriter(@"C:\Users\goldb\Desktop\LogFile.txt", true))
+        {
+            logFile.WriteLine("["
+                + System.DateTime.Now
+                + "]"
+                + message 
+                + "\n");
+        }
+    }
+    //Logic for quitting the application
+    void OnApplicationQuit()
+    {
+        WriteToLog("System Stop");
+    }
+}
